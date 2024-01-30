a8aedf405600b50876fd99cfdafbaae1d1aff78c
roor <02tladbwjd@naver.com>
2024-01-30 15:06:08 +0900

    엔티티 만들기

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'; //리포지토리 주입 데코레이터
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        //리포지토리 주입
+        @InjectRepository(User) private userRepository: Repository<User>,
+    ) {}
+}
